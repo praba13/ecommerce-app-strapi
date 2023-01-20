a88369ec04188292237011d7bf3714cadf7dd9b2
Praba <prama1303@gmail.com>
2023-01-21 00:45:41 +0100

    Categories Component added

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import "./Home.css";
 import Slider from "../../components/Slider/Slider";
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts';
+import Categories from "../../components/Categories/Categories";
 
 const Home = () => {
     return (
         <div className="home">
             <Slider />
             <FeaturedProducts type="featured" />
+            <Categories />
             <FeaturedProducts type="trending" />
         </div>
     );
