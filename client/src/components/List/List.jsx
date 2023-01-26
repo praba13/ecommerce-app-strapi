b18e2020a60f40f70988a7d94c08e03a7000e557
Praba <prama1303@gmail.com>
2023-01-26 23:07:22 +0100

    fetch data from Strapi to categories components

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -1,7 +1,13 @@
+import useFetch from '../../hooks/useFetch';
 import Card from '../Card/Card';
 import './List.scss';
-const List = () => {
+const List = ({ subCats, maxPrice, sort, catId }) => {
 
+    const { data, loading, error } = useFetch(
+        `/products?populate=*&[filters][categories][id]=${catId}`
+    );
+
+    /*
     const data = [
         {
             id: 1,
@@ -37,11 +43,13 @@ const List = () => {
             price: 15
         }
     ];
-
+    */
     return (
-        <div className='list'>{data?.map(item => (
-            <Card item={item} key={item.id} />
-        ))}</div>
+        <div className='list'>
+            {loading ? "loading.." : data?.map(item => (
+                <Card item={item} key={item.id} />
+            ))}
+        </div>
     );
 };
 
